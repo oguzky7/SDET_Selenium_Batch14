647b9ac701cbcebb32b0c4f2cdfabbe14d771bc7
oguzky7 <oguzky7@gmail.com>
2022-12-07 17:44:33 -0500

    12/07/2022

--- src/com/Syntax/Practise/OnlyClockOut.java
+++ src/com/Syntax/Practise/OnlyClockOut.java
@@ -9,17 +9,17 @@ import org.openqa.selenium.support.ui.Select;
 public class OnlyClockOut {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Process has began");
-        Thread.sleep(3300000);
+        Thread.sleep(1300000);
         System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://timeclock.winklerpool.com/mymitc/2/");
         driver.manage().window().maximize();
         driver.findElement(By.xpath("//button[@class ='drop-btn-topmenu']")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//input[@id ='uname']")).sendKeys("4564");
         driver.findElement(By.xpath("//input[@id ='PW']")).sendKeys("123456");
         driver.findElement(By.xpath("//button[@type ='submit']")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//a[contains(@href, 'Clock')]")).click();
         //Thread.sleep(1000);
         //WebElement options = driver.findElement(By.xpath("//select[contains(@onchange, 'check')]"));
